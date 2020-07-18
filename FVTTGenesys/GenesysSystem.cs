88056a5d5023cdb06d5738cb3f6ae85c2ad399fc
Jeff Kessleman <jeffpk@worldwizards.net>
2020-07-18 15:59:41 -0400

    Add project files.

---
+++ FVTTGenesys/GenesysSystem.cs
@@ -0,0 +1,28 @@
+﻿using FVTTBridge.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FVTTGenesys
+{
+    public class GenesysSystem : FoundrySystem
+    {
+        public override Type ActorType
+        {
+            get
+            {
+                return typeof(GenesysActor);
+            }
+        }
+
+        public override Type ActorSheetType
+        {
+            get
+            {
+                return typeof(GenesysActorSheet);
+            }
+        }
+    }
+}
